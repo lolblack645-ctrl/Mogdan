487836059f14251e14e06baf523997defa378ba7
Твое Имя <_email@example.com>
2026-09-20 23:41:14 +0700

    Add gui command

--- src/main/java/com/staqclient/StaqClient.java
+++ src/main/java/com/staqclient/StaqClient.java
@@ -2,32 +2,22 @@ package com.staqclient;
 
 import com.staqclient.ui.ClickGuiScreen;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
-import org.lwjgl.glfw.GLFW;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 public class StaqClient implements ClientModInitializer {
-    public static KeyBinding clickGuiKeyBinding;
-
     @Override
     public void onInitializeClient() {
-        clickGuiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.staqclient.clickgui",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_P,
-                "category.staqclient.general"
-        ));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (clickGuiKeyBinding.wasPressed()) {
-                if (client.player != null) {
-                    client.player.sendMessage(Text.literal("§a[StaqClient] Key pressed! Opening GUI..."), false);
-                }
-                client.setScreen(new ClickGuiScreen());
-            }
+        // Регистрируем клиентскую команду /gui
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("gui")
+                .executes(context -> {
+                    context.getSource().getClient().execute(() -> {
+                        context.getSource().getClient().setScreen(new ClickGuiScreen());
+                    });
+                    return 1;
+                })
+            );
         });
     }
 }
