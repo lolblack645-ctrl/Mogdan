b5dfe74691d03c3605b7f3fa7cc22c5bdcf5c233
Твое Имя <_email@example.com>
2026-09-20 22:59:11 +0700

    Add ClickGuiScreen

---
+++ src/main/java/com/staqclient/ui/ClickGuiScreen.java
@@ -0,0 +1,22 @@
+package com.staqclient.ui;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class ClickGuiScreen extends Screen {
+    public ClickGuiScreen() {
+        super(Text.literal("Staq Client Menu"));
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 40, 0xFFFFFF);
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
+    }
+}
